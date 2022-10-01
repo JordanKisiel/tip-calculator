1fe77f383dcd7f6ddfc8c369345401325efa6cdb
Jordan Kisiel <jordan.kisiel@gmail.com>
2022-10-01 18:19:27 -0400

    Initial HTML done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./*.html",
     "./js/*.js"
   ],
   theme: {
-    extend: {},
+    screens: {
+      //TODO: add screen sizes as needed
+    },
+    extend: {
+      colors: {
+        strongCyan: 'hsl(172, 67%, 45%)',
+        veryDarkCyan: 'hsl(183, 100%, 15%)',
+        darkGrayCyan: 'hsl(186, 14%, 43%)',
+        grayCyan: 'hsl(184, 14%, 56%)',
+        lightGrayCyan: 'hsl(185, 41%, 84%)',
+        veryLightGrayCyan: 'hsl(189, 41%, 97%)',
+        white: 'hsl(0, 0%, 100%)'
+      },
+      fontFamily: {
+        'mono': ['Space Mono', ...defaultTheme.fontFamily.mono]
+      }
+    },
   },
   plugins: [],
 }
