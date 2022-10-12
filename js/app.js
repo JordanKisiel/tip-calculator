61789ab8b2216754e54b2eac75a1d609c111bd02
Jordan Kisiel <jordan.kisiel@gmail.com>
2022-10-11 22:55:06 -0400

    First iteration of totals calculation done

--- js/app.js
+++ js/app.js
@@ -13,7 +13,13 @@ let tipPercentage = '15%';
 
 //event listeners
 tipOptions.forEach(button => button.addEventListener('click', selectPresetTipAmount));
-customTip.addEventListener('blur', setCustomTipAmount);
+customTip.addEventListener('blur', (e) => {
+    setCustomTipAmount(e);
+    calculateTotals();
+});
+billAmount.addEventListener('blur', calculateTotals);
+numOfPeople.addEventListener('blur', calculateTotals);
+
 
 
 //callback functions
@@ -28,22 +34,102 @@ function selectPresetTipAmount(e){
     customTip.value = '';
     
     tipPercentage = e.target.innerHTML;
-    console.log(tipPercentage);
 }
 
 function setCustomTipAmount(e){
-    tipPercentage = e.target.value;
+    //verify valid input (accept both number & percentage format)
+    if(/[^0-9%.]/g.test(e.target.value)){
+        e.target.value = '';
+    }
+    else{
+        tipPercentage = e.target.value;
+        console.log(tipPercentage);
 
-    //set appearance of all presets to inactive
-    tipOptions.forEach(button => setInactiveAppearance(button));
+        //set appearance of all presets to inactive
+        tipOptions.forEach(button => setInactiveAppearance(button));
+    }
+}
 
-    console.log(tipPercentage);
+function calculateTotals(){
+    let bill = billAmount.value;
+    let people = numOfPeople.value;
+    let tip = '';
+
+    console.log(isValidPercentage(tipPercentage));
+
+    if(isValidBillAmount(bill) && isValidPeopleAmount(people) && isValidPercentage(tipPercentage)){
+        //take percent sign off tip
+        let tip = '';
+
+        if(tipPercentage.slice(-1) === '%'){
+            tip = tipPercentage.slice(0, tipPercentage.length - 1);
+        }
+        else{
+            tip = tipPercentage;
+        }
+        
+        let totalPerPerson = Number(bill) * (1 + Number(tip)/100) / Number(people);
+        let tipPerPerson = Number(bill) * (Number(tip)/100) / Number(people);
+
+        displayTotals(totalPerPerson, tipPerPerson);
+    }
+    else{
+        console.log('something went wrong');
+    }
 }
 
 
 
 
 //helper functions
+function displayTotals(total, tip){
+    total = total.toFixed(2);
+    tip = tip.toFixed(2);
+    tipAmount.innerHTML = '$' + tip;
+    totalPerPerson.innerHTML = '$' + total;
+}
+
+function isValidPercentage(percentage){
+    if(percentage.slice(-1) === '%'){
+        if(!Number(percentage.slice(0, percentage.length - 1))){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        if(!Number(percentage)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+function isValidBillAmount(amount){
+    if(!Number(amount)){
+        return false;
+    }
+    return true;
+}
+
+function isValidPeopleAmount(amount){
+    if(!Number(amount)){
+        return false;
+    }
+    else if(!Number.isInteger(Number(amount))){
+        return false;
+    }
+    else if(Number(amount) <= 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
 function setActiveAppearance(button){
     button.classList.toggle('bg-veryDarkCyan');
     button.classList.toggle('bg-strongCyan');
